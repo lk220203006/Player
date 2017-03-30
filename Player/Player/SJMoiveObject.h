ff117a768c544d29ca47c9d0d0836864334ca3e0
lgq <mac@macdeMacBook-Pro.local>
2017-03-30 15:56:15 +0800

    集成ffmpeg

---
+++ Player/Player/SJMoiveObject.h
@@ -0,0 +1,16 @@
+//
+//  SJMoiveObject.h
+//  Player
+//
+//  Created by lgq on 2017/3/30.
+//  Copyright © 2017年 liukun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "avcodec.h"
+#import "avformat.h"
+#import "swscale.h"
+
+@interface SJMoiveObject : NSObject
+
+@end
---
+++ Player/Player/SJMoiveObject.m
@@ -0,0 +1,13 @@
+//
+//  SJMoiveObject.m
+//  Player
+//
+//  Created by lgq on 2017/3/30.
+//  Copyright © 2017年 liukun. All rights reserved.
+//
+
+#import "SJMoiveObject.h"
+
+@implementation SJMoiveObject
+
+@end
